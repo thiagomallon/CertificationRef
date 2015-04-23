40e0df7c11b615e47f47f0d3cd30a908d974de4b
Mallon <thiagomallon@gmail.com>
2015-04-23 12:33:07 -0300

    Updating OODaughterClassTest at '23/04/2015'

--- PHPCertified/tests/object-orientation/OODaughterClassTest.php
+++ PHPCertified/tests/object-orientation/OODaughterClassTest.php
@@ -15,7 +15,7 @@ class OODaughterClassTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(12, $this->daughter->sumNumbers(6, 6));
         $this->assertEquals(16, $this->daughter->finalSumNumbers(8, 8));
     }
-
+    
     public function tearDown()
     {
         $this->daughter = null;
