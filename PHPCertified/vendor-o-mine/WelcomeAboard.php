370760b20a26a2b54eebb49c8b2d1c2224d6bd58
Mallon <thiagomallon@gmail.com>
2015-05-06 13:09:39 -0300

    Updating composer script messages at 06/05/2015

--- PHPCertified/vendor-o-mine/WelcomeAboard.php
+++ PHPCertified/vendor-o-mine/WelcomeAboard.php
@@ -10,12 +10,30 @@ use Composer\Script\Event;
  */
 class WelcomeAboard
 {
+    public static function preInstall(Event $event)
+    {
+        $composer = $event->getIO();
+        $composer->write("\n\n            
+            Wake up matey!\n 
+            Go to the port get a few things I ordered from an old friend..\n
+            by T.Mallon\n\n\n");
+    }
+
     public static function postInstall(Event $event)
     {
         $composer = $event->getIO();
         $composer->write("\n\n            
-            Let's go, matey! There is always\n 
+            Take a look matey! There is always\n 
             room for one more.\n
+            by T.Mallon\n\n\n");
+    }
+
+    public static function preUpdate(Event $event)
+    {
+        $composer = $event->getIO();
+        $composer->write("\n\n            
+            Hey, matey!\n            
+            Go clean up this mess!
             by T.Mallon\n\n\n");
     }
 
@@ -25,6 +43,25 @@ class WelcomeAboard
         $composer->write("\n\n            
             This is the right way, matey!\n
             Keep our sails clean..\n
+            by T.Mallon\n\n\n");
+    }
+
+    public static function preAutoLoadDump(Event $event)
+    {
+        $composer = $event->getIO();
+        $composer->write("\n\n 
+            Damn, matey!\n
+            This old map is making us going in circles.\n
+            Go get a new map for me!\n
+            by T.Mallon\n\n\n");
+    }
+    
+    public static function postAutoLoadDump(Event $event)
+    {
+        $composer = $event->getIO();
+        $composer->write("\n\n           
+            Oh yes, matey!\n
+            Now you are sailing like a butterfly!\n
             by T.Mallon\n\n\n");
     }
 }
