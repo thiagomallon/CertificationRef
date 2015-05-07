3aca194c9ebecb21352e79d62504d658802d9f80
Mallon <thiagomallon@gmail.com>
2015-05-07 12:42:24 -0300

    Deleting inutils at 06/05/2015

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,10 +23,6 @@ class ComposerAutoloaderInit3aa22345ff4e67f473c50028bffc5b80
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit3aa22345ff4e67f473c50028bffc5b80', 'loadClassLoader'));
 
-        $includePaths = require __DIR__ . '/include_paths.php';
-        array_push($includePaths, get_include_path());
-        set_include_path(join(PATH_SEPARATOR, $includePaths));
-
         $map = require __DIR__ . '/autoload_namespaces.php';
         foreach ($map as $namespace => $path) {
             $loader->set($namespace, $path);
