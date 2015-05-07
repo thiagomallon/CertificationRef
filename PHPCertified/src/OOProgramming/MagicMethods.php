553b8a1936f7f0264b5aff324ef26f1a14a306c0
Mallon <thiagomallon@gmail.com>
2015-05-06 23:38:45 -0300

    Updating docs and tests at 06/05/2015

--- PHPCertified/src/OOProgramming/MagicMethods.php
+++ PHPCertified/src/OOProgramming/MagicMethods.php
@@ -4,15 +4,17 @@ namespace App\OOProgramming;
 
 class MagicMethods
 {
-    protected $data = []; // array armazenará propriedades não definidas e seus valores
+    /**
+     * @var Array $data
+     * Armazenará propriedades não definidas e seus valores
+     */
+    protected $data = [];
 
-    private $particular;
-    protected $familiar;
-    public $informal;
-
-    /* método mágico, utilizado para que métodos chamados via a instancia da classe,
+    /**
+     * @method returntype description
+     * método mágico, utilizado para que métodos chamados via a instancia da classe,
      * que não possuam implementação, sejam interceptados - caso chame-se, através de
-     * uma instância dessa classe, um método que não esteja em seu escopo, o método 
+     * uma instância dessa classe, um método que não esteja em seu escopo, o método
      * __call() será executado.
      */
     public function __call($name, $args)
@@ -20,6 +22,9 @@ class MagicMethods
         return $args;
     }
 
+    /**
+     * @method returntype description
+     */
     public function __set($name, $value)
     {
         $this->data[$name] = $value;
@@ -30,8 +35,34 @@ class MagicMethods
         return $this->data[$name];
     }
 
-    /* método é chamado toda vez que instância de classe é chamado como um string. ex:
-     * echo $_magicMethods; 
+    /**
+     * @method returntype description
+     */
+    public function __invoke()
+    {
+
+    }
+
+    /**
+     * @method returntype description
+     */
+    public function __serialize()
+    {
+
+    }
+
+    /**
+     * @method returntype description
+     */
+    public function __sleep()
+    {
+
+    }
+
+    /**
+     * @method returntype description
+     * método é chamado toda vez que instância de classe é chamado como um string. ex:
+     * echo $_magicMethods;
      * Quando ocorre uma chamada assim, o retorno do método __toString será exibido no echo.
      */
     public function __toString()
@@ -39,6 +70,9 @@ class MagicMethods
         return 'Hey, this is not a string, this is the MagicMethods class. Welcome aboard, pirate!';
     }
 
+    /**
+     * @method returntype description
+     */
     public function __debugInfo()
     {
         return [
